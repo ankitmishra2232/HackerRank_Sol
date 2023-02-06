323eff7a59229ff179a6a89ef5c2453be6a3bea7
Ankit Mishra <ankitmishra.in.com@gmail.com>
2023-02-06 20:21:22 +0530

    new question

--- 05-02-2023.py
+++ 05-02-2023.py
@@ -69,3 +69,27 @@ elif catB < catA:
     print('Cat B')
 else:
     print('Mouse C')
+    
+
+# https://www.hackerrank.com/challenges/magic-square-forming/problem?isFullScreen=false
+
+squares = [
+    [[8, 1, 6], [3, 5, 7], [4, 9, 2]], 
+    [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+    [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+    [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+    [[4, 9, 2], [3, 5, 7], [8, 1, 6]], 
+    [[8, 3, 4], [1, 5, 9], [6, 7, 2]], 
+    [[6, 1, 8], [7, 5, 3], [2, 9, 4]], 
+    [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+]
+
+def formingMagicSquare(s):
+    costs=[]
+    for q in squares:
+        cost=0
+        for i in range(3):
+            for j in range(3):
+                cost+=abs(q[i][j]-s[i][j])
+        costs.append(cost)
+    print(min(costs)) 
